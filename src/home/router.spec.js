3ac7fb8aba00162c534c81c649bf758174f24ac6
grischa17 <richard.martens@outlook.com>
2017-05-16 22:42:04 +0200

    first specs, but no assertions

---
+++ src/home/router.spec.js
@@ -0,0 +1,26 @@
+var request = require("request");
+
+var base_url = "http://localhost:3000/"
+
+describe("Hello World Server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function() {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+  
+  describe("GET /user", function() {
+    it("returns status code 200", function() {
+        delete request;
+        
+      request.get(base_url + '/user', function(error, response, body) {
+        expect(response.statusCode).toBe(123);
+        done();
+      });
+    });
+  });
+
+});
